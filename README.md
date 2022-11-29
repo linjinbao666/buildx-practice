6f2d6af0c57b8fdcd3359aef1bf54ec4e5ea83be
liuwa <liuwa.i-i.ai>
2022-11-29 11:11:38 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# instruction
+
+```bash
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm -t docker.io/linjinbao66/buildx-practice:0.0.1 -o type=registry .
+```
+
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+	fmt.Printf("Running in [%s] architecture.\n", runtime.GOARCH)
+}
+
